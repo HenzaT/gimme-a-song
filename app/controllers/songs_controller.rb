59fdf92ec6eeab8bf1cd0fed2341a2afc0a8e13e
Henry Thomas <henrykun95@gmail.com>
2025-02-27 18:21:28 +0000

    added bpm and time sig to new

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,5 +3,8 @@ class SongsController < ApplicationController
   end
 
   def new
+    @song = Song.new
+    @song.time_signature = %w[4/4 3/4 5/4 6/8 7/8 9/8].sample(1).join
+    @song.bpm = rand(60..190)
   end
 end
