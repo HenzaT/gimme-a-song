c89bad028b49f22ea7f0fde05535fcc1c4eb1670
Henry Thomas <henrykun95@gmail.com>
2025-03-21 15:58:15 +0000

    added JS functionality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home new]
+  skip_before_action :authenticate_user!, only: %i[home new new_instrument]
 
   def home
     @two_songs = Song.limit(2)
--- app/javascript/controllers/random-time-sig_controller.js
+++ app/javascript/controllers/random-time-sig_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="random-time-sig"
 export default class extends Controller {
-  static targets = ["time"];
+
+  static targets = [ "time", "timeSigCard", "button" ];
 
   getRandomTime() {
     const times = ["4/4", "5/4", "6/4", "6/8", "9/8", "7/8", "3/4"];
@@ -12,5 +13,9 @@ export default class extends Controller {
   changeTime() {
     const newTime = this.getRandomTime();
     this.timeTarget.innerText = newTime;
+  }
+
+  lockTime() {
+    this.buttonTarget.classList.toggle("d-none");
   }
 }
