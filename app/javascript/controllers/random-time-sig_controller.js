e55c3386f7a7737fad7d4f9c64f92dbe24a4a86c
Henry Thomas <henrykun95@gmail.com>
2025-03-21 19:49:34 +0000

    added toggle lock

--- app/javascript/controllers/random-time-sig_controller.js
+++ app/javascript/controllers/random-time-sig_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="random-time-sig"
 export default class extends Controller {
 
-  static targets = [ "time", "timeSigCard", "button" ];
+  static targets = [ "time", "padlock", "timeSigCard", "button" ];
 
   getRandomTime() {
     const times = ["4/4", "5/4", "6/4", "6/8", "9/8", "7/8", "3/4"];
@@ -16,6 +16,7 @@ export default class extends Controller {
   }
 
   lockTime() {
-    this.buttonTarget.classList.toggle("d-none");
+    this.buttonTarget.toggleAttribute("disabled");
+    // this.padlockTarget.toggleClass();
   }
 }
