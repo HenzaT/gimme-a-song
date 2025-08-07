2bd2ea99ceb2a2bcfa3e65173f2c8db0e90e0b14
Henry Thomas <henrykun95@gmail.com>
2025-08-07 10:33:44 +0100

    more styling

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -3,9 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="loader"
 export default class extends Controller {
 
-  static targets = [ 'footer' ]
+  static targets = [ 'footer', 'htmlBody', 'navbar', 'card', 'button', 'header', 'text', 'instCard' ]
 
-  // connect() {
-  //   this.footerTarget.classList.add('fade');
+  connect() {
+    this.fadeIn(this.headerTarget, 0);
+    this.fadeIn(this.textTarget, 500);
+    this.fadeIn(this.buttonTarget, 600);
+    this.fadeIn(this.cardTarget, 800);
+    this.fadeIn(this.footerTarget, 1000);
+  }
+
+  fadeIn(target, delay) {
+    if (target) {
+      setTimeout(() => {
+        target.classList.add('fade-in')
+      }, delay)
+    }
+  }
+
+  // toggleDarkMode() {
+  //   this.htmlBodyTarget.classList.toggle("dark-mode");
+  //   this.navbarTarget.classList.toggle("dark-mode");
   // }
 }
